4be6d4206eb92f552e455bd392233626fc73eb80
unknown <contactmampbeni@gmail.com>
2021-10-22 15:07:24 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,5 @@
+
+//import {addition} from './math.module';
+import * as MathFunctions from './math.module';
+
+console.log(MathFunctions.addition(4,6))
---
+++ math.module.ts
@@ -0,0 +1,18 @@
+
+    export function addition(x:number,y:number){
+        return x+y;
+    }
+    export function soustraction(x:number,y:number){
+        return x-y;
+    }
+    export function multiplication(x:number,y:number){
+        return x*y;
+    }
+    export function division(x:number,y:number){
+        return x/y;
+    }
+    export function modulo(x:number,y:number){
+        return x%y;
+    }
+
+
